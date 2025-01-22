03289407be6fe65e1f64f7282e85db9b579bc7af
Swiss Tangsatjatham <tintinguy01@gmail.com>
2025-01-23 00:49:47 +0700

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-      domains: [
-          "files.edgestore.dev"
-      ]
-  }
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? 'https://github.com/tintinguy01/pomo-time' : '',
+  basePath: isProd ? 'https://github.com/tintinguy01/pomo-time' : '',
+  output: 'export'
 };
 
 export default nextConfig;
